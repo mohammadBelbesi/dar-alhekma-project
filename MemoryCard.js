eb2f57ec766b60363c325744e931c514836f7184
mohammadBelbesi <my_1999@live.com>
2024-12-16 13:25:37 +0200

    conflict solved

--- MemoryCard.js
+++ MemoryCard.js
@@ -1,15 +1,4 @@
 // here we go
 // fares
 // mazen
-// mohammadBelbesi
-// ahmad 
-<HTMLAnchorElement>
-<bady>
-<a1>hi im fares </a1>
-
-
-</bady>
-
-
-
-</HTMLAnchorElement>
+// mohammadBelbesi + ahmad
